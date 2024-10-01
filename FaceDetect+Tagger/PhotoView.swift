ce59adc7e28b4430a0492c74c68fbdedd58bdf9c
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-09-30 18:55:40 -0600

    misc updates

--- FaceDetect+Tagger/PhotoView.swift
+++ FaceDetect+Tagger/PhotoView.swift
@@ -67,33 +67,59 @@ struct PhotoView: View {
                         }
                     }
                     .listRowInsets(EdgeInsets())
+                    .gesture(
+                        DragGesture(minimumDistance: 30)
+                            .onEnded { value in
+                                print("gesture captured")
+                                guard let index = viewModel.faces.firstIndex(where: { $0.isSelected }) else { return }
+                                
+                                // Check if the drag was horizontal
+                                if abs(value.translation.width) > abs(value.translation.height) {
+                                    // Deselect the current face
+                                    viewModel.faces[index].isSelected = false
+                                    
+                                    // Update the index based on the swipe direction
+                                    let newIndex = value.translation.width > 0 ? index + 1 : index - 1
+                                    
+                                    // Ensure the new index is within bounds
+                                    if viewModel.faces.indices.contains(newIndex) {
+                                        viewModel.faces[newIndex].isSelected = true
+                                    } else {
+                                        // Revert to the current index if out of bounds
+                                        viewModel.faces[index].isSelected = true
+                                    }
+                                }
+                            }
+                    )
                     
-                    Section{
-                        ScrollView(.horizontal) {
-                            HStack{
-                                ForEach(viewModel.faceThumbnails, id: \.id) { thumbnail in
-                                    Image(uiImage: thumbnail.image)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 50, height: 50)
-                                        .clipShape(Circle())
-                                        .padding(4)
-                                        .overlay(
-                                            Circle()
-                                                .stroke(thumbnail.isSelected ? Color.blue : Color.clear, lineWidth: 2)
-                                        )
-                                        .onTapGesture {
-                                            // Deselect all thumbnails first
-                                            viewModel.deselectAllThumbnails()
-                                            // Select only the tapped thumbnail
-                                            if let index = viewModel.faceThumbnails.firstIndex(where: { $0.id == thumbnail.id }) {
-                                                viewModel.faceThumbnails[index].isSelected = true
+                   
+                        Section{
+                            ScrollView(.horizontal) {
+                                HStack(spacing: 10){
+                                    ForEach(viewModel.faces, id: \.id) { thumbnail in
+                                        Image(uiImage: thumbnail.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 70, height: 70)
+                                            .clipShape(Circle())
+                                            .padding(4)
+                                            .overlay(
+                                                Circle()
+                                                    .stroke(thumbnail.isSelected ? Color.blue : Color.clear, lineWidth: 2)
+                                            )
+                                            .onTapGesture {
+                                                // Deselect all thumbnails first
+                                                viewModel.deselectAllThumbnails()
+                                                // Select only the tapped thumbnail
+                                                if let index = viewModel.faces.firstIndex(where: { $0.id == thumbnail.id }) {
+                                                    viewModel.faces[index].isSelected = true
+                                                }
                                             }
-                                        }
+                                    }
                                 }
                             }
                         }
-                    }
+                    
                     
                     
                     Section{
@@ -109,18 +135,29 @@ struct PhotoView: View {
                             }
                             .autocorrectionDisabled(true)
                             .onSubmit{
+                                if let firstResult = viewModel.searchResults.first,
+                                   let index = viewModel.searchResults.firstIndex(where: { $0.id == firstResult.id }) {
+                                    
+                                    // if contacto exite
+                                    // agregar face selecctionada a contacto
+                                    // else (contacto no existe)
+                                    // crear contacto con texto y thumnail seleccionado
+                                    //viewModel.contacts[index] =
+                                    //viewModel.selectedNames.append(firstResult)
+                                    //viewModel.searchResults[index].selected = true
+                                }
+                                
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                                     withAnimation{
                                         viewModel.searchText = ""
                                     }
                                 }
-                                if let firstResult = viewModel.searchResults.first,
-                                   let index = viewModel.searchResults.firstIndex(where: { $0.id == firstResult.id }) {
-                                    viewModel.selectedNames.append(firstResult)
-                                    viewModel.searchResults[index].selected = true
-                                }
                             }
+                            
+                        // on swipe seleccionar siguiente contacto // seleccionar previo contacto
+                        
                     }
+                    
 
                     Section{
                         ForEach(viewModel.searchResults){ contact in
@@ -129,13 +166,15 @@ struct PhotoView: View {
                         }
                     }
                 }
+                
+                .task {
+                    await viewModel.detectFaces()
+                }
             }
-            .task {
-                await viewModel.detectFaces()
-            }
+            
             .onDisappear {
                 viewModel.detectedFaces.removeAll()
-                viewModel.faceThumbnails.removeAll()
+                viewModel.faces.removeAll()
             }
         }
     }
@@ -164,10 +203,18 @@ extension Contact {
     }
 }
 
-struct FaceThumbnail: Identifiable, Hashable {
+class Face: Identifiable {
     var id = UUID()
     var image: UIImage
-    var isSelected: Bool = false
+    var isSelected: Bool
+    var faceQuality: Float?
+    
+    init(id: UUID = UUID(), image: UIImage, isSelected: Bool = false, faceQuality: Float? = nil) {
+        self.id = id
+        self.image = image
+        self.isSelected = isSelected
+        self.faceQuality = faceQuality
+    }
 }
 
 extension PhotoView {
@@ -176,19 +223,25 @@ extension PhotoView {
         @Published var showSheet = false
         @Published var showAddNameSheet = false
         @Published var showPhotosPicker = false
+        
         @Published var contacts: [Contact] = []
+        @Published var faces: [Face] = []
+        
         @Published var imageItem: UIImage = UIImage()
         @Published var selectedItem: PhotosPickerItem?
         @Published var newContactName = ""
         @Published var searchResults: [Contact] = []
         @Published var selectedNames: [Contact] = []
         @Published var searchText = ""
-        @Published var faceThumbnails: [FaceThumbnail] = []
+
         @Published var imageWithDetections: UIImage?
         @Published var croppedImages: [CGImage] = []
         
         var detectedFaces: [VNFaceObservation] = []
         var photoGalleryImages: [UIImage] = []
+        
+        var segments = 1
+        var segmentXOffset = 0.0
 
         
         init(){
@@ -268,6 +321,8 @@ extension PhotoView {
             
             do {
                 try handler.perform([request])
+                segments = 1
+                segmentXOffset = 0
                 await generateFaceThumbnails()
             } catch {
                 print("Error performing face detection: \(error)")
@@ -283,11 +338,12 @@ extension PhotoView {
             if self.detectedFaces.isEmpty {
                 let imageWidth = cgImage.width
                 let imageHeight = cgImage.height
+                segments = 2
                 
                 for segment in 0..<2 {
                     let segmentWidth = imageWidth / 2
-                    let xOffset = segment * segmentWidth
-                    let segmentRect = CGRect(x: xOffset, y: 0, width: segmentWidth, height: imageHeight)
+                    segmentXOffset = CGFloat(segment * segmentWidth)
+                    let segmentRect = CGRect(x: Int(segmentXOffset), y: 0, width: segmentWidth, height: imageHeight)
                     
                     // Create a cropped CGImage for the segment
                     croppedImages.append(cgImage.cropping(to: segmentRect)!)
@@ -300,10 +356,13 @@ extension PhotoView {
                     } catch {
                         print("Error performing face detection: \(error)")
                     }
+                    
+
+                    
                     print("Number of faces detected on second round: \(detectedFaces.count)")
                 }
             }
-            addFaceRectsToImage()
+            //addFaceRectsToImage()
         }
            
         
@@ -314,25 +373,53 @@ extension PhotoView {
                 return
             }
 
-            let imageSize = CGSize(width: imageItem.size.width, height: imageItem.size.height)
+            let imageSize = CGSize(width: imageItem.size.width/CGFloat(segments), height: imageItem.size.height)
 
+            
+            
             for face in detectedFaces {
                 let boundingBox = face.boundingBox
 
-                let box = CGRect(
-                    x: boundingBox.origin.x * imageSize.width,
-                    y: (1 - boundingBox.origin.y - boundingBox.height) * imageSize.height,
-                    width: boundingBox.width * imageSize.width,
-                    height: boundingBox.height * imageSize.height
-                )
+                let scaleFactor: CGFloat = 1.8 // Adjust this value to increase the box size (1.0 = original size)
 
-                if let cgCroppedImage = cgImage.cropping(to: box) {
+                let scaledBox = CGRect(
+                    x: boundingBox.origin.x * imageSize.width - (boundingBox.width * imageSize.width * (scaleFactor - 1)) / 2,
+                    y: (1 - boundingBox.origin.y - boundingBox.height) * imageSize.height - (boundingBox.height * imageSize.height * (scaleFactor - 1)) / 2,
+                    width: boundingBox.width * imageSize.width * scaleFactor,
+                    height: boundingBox.height * imageSize.height * scaleFactor
+                )
+                
+                if let cgCroppedImage = cgImage.cropping(to: scaledBox) {
                     let thumbnail = UIImage(cgImage: cgCroppedImage)
-                    faceThumbnails.append(FaceThumbnail(image: thumbnail))
+                    faces.append(Face(image: thumbnail))
                 } else {
                     print("Failed to crop image for boundingBox: \(boundingBox)")
                 }
             }
+        }
+        
+        func addFaceQuality(){
+            let request = VNDetectFaceCaptureQualityRequest()
+            
+            #if targetEnvironment(simulator)
+            let allDevices = MLComputeDevice.allComputeDevices
+            for device in allDevices {
+                request.setComputeDevice(device, for: .main)
+            }
+            #endif
+            
+            for face in faces {
+                let requestHandler = VNImageRequestHandler(cgImage: face.image as! CGImage)
+                do {
+                    try requestHandler.perform([request])
+                } catch {
+                    print("Can't make the request due to \(error)")
+                }
+                guard let results = request.results else { return }
+                face.faceQuality = results.first?.faceCaptureQuality
+            }
+            
+            
         }
         
         private func sortFaceThumbnails(_ faces: [VNFaceObservation], imageSize: CGSize) -> [VNFaceObservation] {
@@ -357,7 +444,7 @@ extension PhotoView {
         }
         
         func deselectAllThumbnails() {
-            faceThumbnails = faceThumbnails.map { FaceThumbnail(id: $0.id, image: $0.image, isSelected: false) }
+            faces = faces.map { Face(id: $0.id, image: $0.image, isSelected: false) }
         }
         
         func addFaceRectsToImage(){
@@ -375,7 +462,7 @@ extension PhotoView {
                 let boundingBox = face.boundingBox
                 let rect = CGRect(
                     x: boundingBox.origin.x * imageSize.width,
-                    y: (1 - boundingBox.origin.y - boundingBox.size.height) * imageSize.height,
+                    y: (1 - boundingBox.origin.y - boundingBox.size.height) * imageSize.height/2,
                     width: boundingBox.size.width * imageSize.width,
                     height: boundingBox.size.height * imageSize.height
                 )
